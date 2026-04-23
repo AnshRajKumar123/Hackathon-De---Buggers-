6739fe99367a6f691fa4411a6780666543e2c6b4
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-04-23 14:41:02 +0530

    Golu - commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '/'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
 
-const App = () => {
-  return (
-    <>
-      
-    </>
-  )
+// Import your Publishable Key from the .env file
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
