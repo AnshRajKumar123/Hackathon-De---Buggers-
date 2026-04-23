ec0b8fb856a7d30f425f1a705d9b64086b7df11c
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-04-23 14:52:45 +0530

    Commit set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './ComponentJSX/Navbar'
 import Home from './ComponentJSX/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
